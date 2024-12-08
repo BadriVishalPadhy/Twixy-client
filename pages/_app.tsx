915b198c887e388f40e75583c3f0b16ca80a766a
Badri-Vishal <badrivishalpadhy@gmail.com>
2024-12-08 12:22:02 +0530

    g-auth

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,6 +1,9 @@
 import "@/styles/globals.css";
 import type { AppProps } from "next/app";
+import { GoogleOAuthProvider } from '@react-oauth/google';
 
 export default function App({ Component, pageProps }: AppProps) {
+  <GoogleOAuthProvider clientId="<your_client_id>">
   return <Component {...pageProps} />;
+  </GoogleOAuthProvider>
 }
